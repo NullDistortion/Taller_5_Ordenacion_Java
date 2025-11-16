072d66cf70d6fbcadbfede650dfe89fe8e9c5d75
Pedro27-J <pedro.l.jimenez@unl.edu.ec>
2025-11-16 11:00:15 -0500

    Actualizacion de la Clase SelectionSort

--- Taller_Ordenacion/src/main/java/ed/u2/sorting/SelectionSort.java
+++ Taller_Ordenacion/src/main/java/ed/u2/sorting/SelectionSort.java
@@ -1,4 +1,52 @@
 package ed.u2.sorting;
 
-public class SelectionSort {
+// Aqui debe buscar el numero menor y colocarlo en el inicio
+public final class SelectionSort {
+
+    private SelectionSort() {}
+
+    public static void sort(int[] arreglo, boolean mostrar) {
+
+        int cambios = 0;
+
+        for (int i = 0; i < arreglo.length - 1; i++) {
+
+            int posicionDelMenor = i;
+
+            if (mostrar) {
+                System.out.println("Busqueda del numero mas pequeño desde la pocion" + i);
+            }
+
+            // Aqui va a buscar el numero menor
+            for (int j = i + 1; j < arreglo.length; j++) {
+                if (arreglo[j] < arreglo[posicionDelMenor]) {
+                    posicionDelMenor = j;
+                }
+            }
+
+            // Aqui es para ver si encuentra un numero mas pequeño lo va a cambiar x ese
+            if (posicionDelMenor != i) {
+                if (mostrar) {
+                    System.out.println("Intercambiando " + arreglo[i] + " con " + arreglo[posicionDelMenor]);
+                }
+
+                int aux = arreglo[i];
+                arreglo[i] = arreglo[posicionDelMenor];
+                arreglo[posicionDelMenor] = aux;
+
+                cambios++;
+            }
+
+            if (mostrar) {
+                System.out.print("Estado del arreglo: ");
+                SortingUtils.printArray(arreglo);
+                System.out.println();
+            }
+        }
+
+        //Aqui debo mostrar los cambios que se hicieron en todos BUBBLE INSERTION Y SELECTION
+        if (mostrar) {
+            System.out.println("Los cambios que se hizo aqui son: " + cambios);
+        }
+    }
 }
